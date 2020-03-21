302dd9265c89bca72689d4360bc73a57a31ec05f
masashi yoshikawa <earnest.marshi@gmail.com>
2020-03-21 12:31:26 +0900

    filter

--- piano/src/filter.rs
+++ piano/src/filter.rs
@@ -18,8 +18,8 @@ impl<T: Clone> RingBuffer<T> {
     }
 
     fn push(&mut self, x: T) {
-        self.v[(self.i - 1) & self.n] = x;
-        self.i = (self.i - 1) % self.n;
+        self.v[(self.i + self.n - 1) % self.n] = x;
+        self.i = (self.i + self.n - 1) % self.n;
     }
 }
 
@@ -31,10 +31,10 @@ struct RingBufferIter<'a, T> {
 impl<'a, T: Copy> Iterator for RingBufferIter<'a, T> {
     type Item = T;
     fn next(&mut self) -> Option<T> {
-        if self.i > self.buf.n {
+        if self.i >= self.buf.n {
             None
         } else {
-            let ret: T = self.buf.v[(self.i - self.buf.i) % self.buf.n];
+            let ret: T = self.buf.v[(self.i + self.buf.i) % self.buf.n];
             self.i += 1;
             Some(ret)
         }
@@ -45,5 +45,29 @@ pub struct Filter {}
 
 #[test]
 fn ring_buffer_work() {
-    // TODO
+    let mut buf = RingBuffer::new(3, 0);
+    let mut iter = buf.iter();
+
+    assert_eq!(Some(0), iter.next());
+    assert_eq!(Some(0), iter.next());
+    assert_eq!(Some(0), iter.next());
+    assert_eq!(None, iter.next());
+
+    buf.push(1);
+    buf.push(2);
+    buf.push(3);
+    let mut iter = buf.iter();
+
+    assert_eq!(Some(3), iter.next());
+    assert_eq!(Some(2), iter.next());
+    assert_eq!(Some(1), iter.next());
+    assert_eq!(None, iter.next());
+
+    buf.push(4);
+    let mut iter = buf.iter();
+
+    assert_eq!(Some(4), iter.next());
+    assert_eq!(Some(3), iter.next());
+    assert_eq!(Some(2), iter.next());
+    assert_eq!(None, iter.next());
 }
